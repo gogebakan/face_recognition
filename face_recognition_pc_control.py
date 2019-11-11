1684e9d29e9c701b19192eb1c9b26ad80ff5ec8f
gogebakan <ummugulsum02@gmail.com>
2019-11-11 16:04:46 +0900

    make pc sleep whenever unknown person sits in front of my pc

---
+++ face_recognition_pc_control.py
@@ -0,0 +1,85 @@
+import face_recognition
+import cv2
+import os
+import numpy as np
+
+# This is a super simple (but slow) example of running face recognition on live video from your webcam.
+# There's a second example that's a little more complicated but runs faster.
+
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Load a sample picture and learn how to recognize it.
+ummu_image = face_recognition.load_image_file("/Users/alisher/Desktop/known_face/Ummu.jpg")
+ummu_face_encoding = face_recognition.face_encodings(ummu_image)[0]
+
+alisher_image = face_recognition.load_image_file("/Users/alisher/Desktop/known_face/Alisher.jpg")
+alisher_face_encoding = face_recognition.face_encodings(alisher_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [ummu_face_encoding, alisher_face_encoding]
+known_face_names = ["Ummu", "Alisher"]
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+
+    # Find all the faces and face enqcodings in the frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+        name = "Unknown"
+
+        # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        if name == "Unknown":
+            os.system("pmset sleepnow")
+
+        # matched_names = []
+        # for i, face_dist in enumerate(face_distances):
+        #     name_ = known_face_names[i]
+        #     matched_names.append(name_)
+        #
+        # print(matched_names)
+        # if "Alisher" not in matched_names and "Ummu" not in matched_names:
+        #     os.system("pmset sleepnow")
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()

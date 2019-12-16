5b96a8abfc391a227adf1e47b19cd752bd1c3bfb
gogebakan <ummugulsum02@gmail.com>
2019-12-16 15:13:58 +0900

    update

--- face_recognition_pc_control.py
+++ face_recognition_pc_control.py
@@ -2,76 +2,57 @@ import face_recognition
 import cv2
 import os
 import numpy as np
+import time
+import datetime
 
-# This is a super simple (but slow) example of running face recognition on live video from your webcam.
-# There's a second example that's a little more complicated but runs faster.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-# Load a sample picture and learn how to recognize it.
 ummu_image = face_recognition.load_image_file("/Users/alisher/Desktop/known_face/Ummu.jpg")
 ummu_face_encoding = face_recognition.face_encodings(ummu_image)[0]
 
-alisher_image = face_recognition.load_image_file("/Users/alisher/Desktop/known_face/Alisher.jpg")
-alisher_face_encoding = face_recognition.face_encodings(alisher_image)[0]
+merve_image = face_recognition.load_image_file("/Users/alisher/Desktop/known_face/Merve.jpg")
+merve_face_encoding = face_recognition.face_encodings(merve_image)[0]
 
-# Create arrays of known face encodings and their names
-known_face_encodings = [ummu_face_encoding, alisher_face_encoding]
-known_face_names = ["Ummu", "Alisher"]
+known_face_encodings = [ummu_face_encoding, merve_face_encoding]
+known_face_names = ["Ummu", "Merve"]
+
+unknown_face_dir = "/Users/alisher/Desktop/unknown_faces/"
+if not os.path.exists(unknown_face_dir):
+    os.makedirs(unknown_face_dir)
 
 while True:
-    # Grab a single frame of video
     ret, frame = video_capture.read()
-
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_frame = frame[:, :, ::-1]
 
-    # Find all the faces and face enqcodings in the frame of video
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    # Loop through each face in this frame of video
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    if len(face_encodings) == 0:
+        pass
+    else:
+        names = ["Unknown"] * len(face_encodings)
+        for i, ((top, right, bottom, left), face_encoding) in enumerate(zip(face_locations, face_encodings)):
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
 
-        name = "Unknown"
+            if matches[best_match_index]:
+                names[i] = known_face_names[best_match_index]
 
-        # If a match was found in known_face_encodings, just use the first one.
-        # if True in matches:
-        #     first_match_index = matches.index(True)
-        #     name = known_face_names[first_match_index]
-
-        # Or instead, use the known face with the smallest distance to the new face
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
-
-        if name == "Unknown":
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        if all(name == 'Unknown' for name in names):
+            for (top, right, bottom, left) in face_locations:
+                ts = time.time()
+                st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+                unknown_face = frame[top:bottom, left:right]
+                cv2.imwrite(unknown_face_dir + st + '.jpg', unknown_face)
             os.system("pmset sleepnow")
+            exit()
 
-        # matched_names = []
-        # for i, face_dist in enumerate(face_distances):
-        #     name_ = known_face_names[i]
-        #     matched_names.append(name_)
-        #
-        # print(matched_names)
-        # if "Alisher" not in matched_names and "Ummu" not in matched_names:
-        #     os.system("pmset sleepnow")
-
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        for (top, right, bottom, left), name in zip(face_locations, names):
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
     # Display the resulting image
     cv2.imshow('Video', frame)
